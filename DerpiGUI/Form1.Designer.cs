680be1637eb8da9100c87dc44d2def5542f139c6
BuildTools <unconfigured@null.spigotmc.org>
2019-01-07 12:51:01 -0800

    First commit

---
+++ DerpiGUI/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace DerpiGUI
+{
+    partial class DerpiGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DerpiGUI));
+            this.output = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.download = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.filename = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.Window;
+            this.output.Location = new System.Drawing.Point(35, 301);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(461, 148);
+            this.output.TabIndex = 4;
+            // 
+            // Search
+            // 
+            this.Search.Enabled = false;
+            this.Search.Location = new System.Drawing.Point(421, 224);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 7;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(518, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(412, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(35, 49);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Input.Size = new System.Drawing.Size(461, 97);
+            this.Input.TabIndex = 5;
+            this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Results:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(298, 253);
+            this.trackBar1.Maximum = 4;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(214, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(384, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Rating";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(564, 449);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(321, 33);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // download
+            // 
+            this.download.Enabled = false;
+            this.download.Location = new System.Drawing.Point(421, 462);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(75, 23);
+            this.download.TabIndex = 14;
+            this.download.Text = "Download";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.download_ClickAsync);
+            // 
+            // filename
+            // 
+            this.filename.Location = new System.Drawing.Point(35, 174);
+            this.filename.Name = "filename";
+            this.filename.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.filename.Size = new System.Drawing.Size(204, 20);
+            this.filename.TabIndex = 15;
+            this.filename.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Filename";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Input";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Upload date",
+            "Score",
+            "Wilson Score",
+            "Relevance",
+            "Random"});
+            this.comboBox1.Location = new System.Drawing.Point(356, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 21);
+            this.comboBox1.TabIndex = 18;
+            this.comboBox1.SelectedIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(311, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Sort by";
+            // 
+            // DerpiGUI
+            // 
+            this.AcceptButton = this.Search;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 497);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DerpiGUI";
+            this.Text = "DerpiGUI";
+            this.Load += new System.EventHandler(this.DerpiGUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button download;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox filename;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DerpiGUI/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace DerpiGUI
+{
+    public partial class DerpiGUI : Form
+    {
+        public DerpiGUI()
+        {
+            InitializeComponent();
+        }
+
+        private DerpiObject.Rootobject deserializeJSON(string str)
+        {
+           
+                return JsonConvert.DeserializeObject<DerpiObject.Rootobject>(str);
+ 
+        }
+
+        private static async Task<string> Derpibooru(string link)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(link).ConfigureAwait(continueOnCapturedContext: false) ;
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+
+                catch (HttpRequestException e)
+                {
+                    //lazy work around, displays 0 results when user puts in invalid search. Will later fix!
+                    HttpResponseMessage response = await client.GetAsync("https://derpibooru.org/search.json?q=pinkie+pie+AND+NOT+pinkie+pie").ConfigureAwait(continueOnCapturedContext: false);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+            }
+
+        }
+
+       
+        private static async Task DownloadFile(Uri fileUri, string extension, string name, string address)
+        {
+            using (var client = new HttpClient())
+            using (var response = await client.GetAsync(fileUri))
+            {
+                response.EnsureSuccessStatusCode();
+                var stream = await response.Content.ReadAsStreamAsync();
+
+                using (var fileStream = File.Create($@"{address}\{name}.{extension}"))
+                {
+                    stream.CopyTo(fileStream);
+                }
+            }
+        }
+
+        private void DerpiGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            DerpiObject.Rootobject results = deserializeJSON(Derpibooru(rating(Input.Text, 1, GetSort())).Result);
+            List<DerpiObject.Search> searches = new List<DerpiObject.Search>();
+            searches.AddRange(results.search.ToList());
+            Random rand = new Random();
+            DerpiObject.Search chosenImage = searches.ElementAt(rand.Next(searches.Count));
+            
+            label1.Text = $"Total Results: {results.total.ToString()}";
+            if (results.total == 0)
+            {
+                output.Text = "No results! Either your tags are misspelled or no images match your search.";
+            }
+            else
+            {
+                var pony = chosenImage.tags;
+                Uri clocky = new Uri($"https:{chosenImage.image}");
+                string extensions = $"https://derpicdn.net/img/view/{chosenImage.created_at.Date.ToString("yyyy/M/d")}/{chosenImage.id}.{chosenImage.original_format}";
+                pictureBox1.Load($"https:{chosenImage.image}");
+                richTextBox1.Text = extensions;
+                output.Text = pony;
+            }
+        }
+        //this function prevents users from submitting an empty query
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+            if(Input.Text == "")
+            {
+                Search.Enabled = false;
+            }
+            else if(Input.Text != "")
+            {
+                Search.Enabled = true;
+            }
+            
+        }
+
+        public string rating(string tags, int pageNumber, string sort)
+        {
+            string rating = "https://derpibooru.org/search.json?q=pinkie+pie";
+           switch(trackBar1.Value)
+            {
+                case 1:
+                    rating = $"https://derpibooru.org/search.json?q={tags}+AND+safe&filter_id=160747&sf={sort}&sd=desc&perpage=50&page={pageNumber}";
+                    
+                    break;
+                case 2:
+                    rating = $"https://derpibooru.org/search.json?q={tags}+AND+%28safe+OR+suggestive%29&filter_id=56027&sf={sort}&sd=desc&perpage=50&page={pageNumber}";
+                   
+                    break;
+                case 3:
+                    rating = $"https://derpibooru.org/search.json?q={tags}+AND+%28safe+OR+questionable+OR+suggestive%29&filter_id=56027&sf={sort}&sd=desc&perpage=50&page={pageNumber}";
+                    
+                    break;
+                case 4:
+                    rating = $"https://derpibooru.org/search.json?q={tags}&key=TSgfZUMEhyV7YdzBC-sD&filter_id=56027&sf={sort}&sd=desc&perpage=50&page={pageNumber}";
+               
+                    break;
+                
+            }
+            return rating;
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            switch (trackBar1.Value)
+            {
+                case 1:
+                   
+                    label2.Text = "Safe";
+                    break;
+                case 2:
+                   
+                    label2.Text = "Suggestive";
+                    break;
+                case 3:
+                    
+                    label2.Text = "Questionable";
+                    break;
+                case 4:
+                   
+                    label2.Text = "Everything";
+                    break;
+
+            }
+        }
+        //makes the link under image clickable
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+            
+        }
+
+        private async void download_ClickAsync(object sender, EventArgs e)
+        {
+            
+            string location = "no path";
+            output.Text = "Beginning...";
+            DerpiObject.Rootobject response = deserializeJSON(Derpibooru(rating(Input.Text, 0, GetSort())).Result);
+            List<DerpiObject.Search> searches = new List<DerpiObject.Search>();
+            int num_pages = response.total / 50;
+
+            if(response.total % 50 > 0)
+            {
+                num_pages++;
+            }
+            int u = 0;
+            if (response.total == 0)
+            {
+                output.Text = "Nothing to download!";
+            }
+            else  if (response.total >= 50)
+            {
+                folderBrowserDialog1.ShowDialog();
+                location = folderBrowserDialog1.SelectedPath;
+                for (int pages = 1; pages <= (num_pages); pages++)
+                {
+                    
+                    response = deserializeJSON(Derpibooru(rating(Input.Text, pages,GetSort())).Result);
+                    searches.AddRange(response.search.ToList());
+                    
+                    
+                    foreach (DerpiObject.Search i in searches)
+                    {
+                        Uri link = new Uri($"https:{i.image}");
+                        
+                        await DownloadFile(link, i.original_format, $"testing{u}", location);
+                        u++;
+                        
+                            output.Text = $"{u} out of {response.total}";
+                        
+                    }
+                    searches.Clear();
+                }
+            }
+            else if (response.total > 0 && response.total <= 50)
+            {
+                folderBrowserDialog1.ShowNewFolderButton = true;
+                folderBrowserDialog1.ShowDialog();
+                location = folderBrowserDialog1.SelectedPath;
+                searches.AddRange(response.search.ToList());
+                foreach (DerpiObject.Search i in searches)
+                {
+                    Uri link = new Uri($"https:{i.image}");
+                    output.Text = "Downloading!";
+                    await DownloadFile(link, i.original_format, $"{filename.Text.Replace("*", u.ToString())}", location);
+                    u++;
+                    if (u <= response.total)
+                    {
+                        output.Text = $"{u} out of {response.total}";
+                    }
+                }
+            }
+
+        }
+        //checks for correct filename
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (filename.Text == "" || Input.Text == "")
+            {
+                download.Enabled = false;
+            }
+            else if (filename.Text != "" && Input.Text != "" && filename.Text.Contains("*"))
+            {
+                output.Text = "Ready to go!";
+                download.Enabled = true;
+            }
+            else if (filename.Text != "" && Input.Text != "" && !filename.Text.Contains("*"))
+            {
+                output.Text = "Please put an asterisk (*) within the filename.";
+            }
+        }
+
+        private string GetSort()
+        {
+            string result = "score";
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    result = "created_at";
+                    break;
+                case 1:
+                    result = "wilson";
+                    break;
+                case 2:
+                    result = "relevance";
+                    break;
+                case 3:
+                    result = "random%3A1096362";
+                    break;
+            }
+            return result;
+        }
+    }
+    }
+
